c7fdbee9188ba8d6b96444a41acb299e765b6f0d
Carlos Quijano <quijanoo12345@gmail.com>
2023-05-24 11:43:29 -0600

    proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from 'react';
+import Dashboard from './componenets/Dashbaord/Dashboard' ;
+import Hightlights from './componenets/Hightlights/Hightlights';
+import NextDays from './componenets/NextDays/NextDays';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+function DataApp() {
+  const API_KEY = process.env.REACT_APP_API_KEY
+  const [currentWeather  , setCurrentWeather] = useState({})
+  const  [nextdays  , setNextdays] = useState([])
+  const [city ,  setCity] = useState('10001')
+  const [temperaturefromat , setTumperature] = useState('c') 
+
+  const getCurrentWeather = useCallback(async (city) => {
+    const url = 'https://api.weatherapi.com/v1/current.json?key=' + API_KEY + '&q=' + city;
+
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      setCurrentWeather(data);
+    } catch {
+      console.log('There is a problem fetching data');
+    }
+  }, [API_KEY]);
+
+  const futureWeather = useCallback(async () => {
+    const url = 'https://api.weatherapi.com/v1/forecast.json?key=' + API_KEY + '&q=' + city + '&days=3&aqi=no&alerts=no';
+
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      setNextdays(data.forecast.forecastday);
+    } catch {
+      console.log('There is a problem with the API');
+    }
+  }, [API_KEY, city]);
+
+    useEffect(() => {
+      getCurrentWeather(city);
+      futureWeather();
+    }, [city, getCurrentWeather, futureWeather]);
+return (
+    <div className="App w-[90%] rounded-[45px]  mx-auto  bg-[#212A3E] ">
+      <div className='flex  flex-col lg:flex-row max-h-full '>
+        <Dashboard currentWeather = {currentWeather} setCity={setCity} temperaturefromat = {temperaturefromat} setTumperature  = {setTumperature}/>
+        <div className='flex flex-col min-w-[70%] max-h-full bg-[#F1F6F9] rounded-[50px]'>
+          <NextDays nextdays = {nextdays} temperaturefromat = {temperaturefromat} />
+          <Hightlights currentWeather = {currentWeather}  />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default DataApp;
---
+++ src/componenets/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='text-black text-center bg-[#000000] text-[14px] px-3 py-8 lg:p-0 lg:mt-auto md:mt-auto'>
+        Create by Carlos Quijano, design from <a className='text-blue-500 font-bold' href='https://devchallenges.io/'>devchallenges.io</a></div>
+  )
+}
+
+export default Footer
